9a1d18fd46a9851af20ae708ec4cd733fdbaa476
Steven Chye <z5257742@ad.unsw.edu.au>
2022-06-06 16:44:29 +1000

    Fixed modal and condensed code

--- src/components/ColourModal.js
+++ src/components/ColourModal.js
@@ -17,6 +17,12 @@ function ColourModal () {
     const c1 = localStorage.getItem('colour1')
     const c2 = localStorage.getItem('colour2')
     const c3 = localStorage.getItem('colour3')
+
+    const colourData = [
+        {c1: '#230078', c2: '#FDF3E5', c3: '#8CB41E'},
+        {c1: '#9381FF', c2: '#FFEEDD', c3: '#B8B8FF'},
+        {c1: '#D4A373', c2: '#FAEDCD', c3: '#606C38'},
+    ]
     
     if (c1 === null || c2 === null || c3 === null) {
         localStorage.setItem('colour1', '#230078')
@@ -65,50 +71,27 @@ function ColourModal () {
 
                     <div className="px-4 py-10 sm:px-6 sm:flex sm:flex-column sm:center">
                         <div style={{display: 'flex', flexDirection: 'column'}}>
-                            <button
-                                type="button"
-                                style={{gap: 10}}
-                                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-200 text-base font-medium text-black hover:bg-orange-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                onClick={() => {
-                                    setOpen(false)
-                                    setColour('#230078', '#FDF3E5', '#8CB41E')
-                                    navigate('/home')
-                                }}
-                            >
-                                <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: '#230078'}}>#230078</div>
-                                <div className="flex flex-col justify-center text-black rounded-md w-32 h-24" style={{backgroundColor: '#FDF3E5'}}>#FDF3E5</div>
-                                <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: '#8CB41E'}}>#8CB41E</div>
-                            </button>
-                            <br />
-                            <button
-                                type="button"
-                                style={{gap: 10}}
-                                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-200 text-base font-medium text-black hover:bg-orange-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                onClick={() => {
-                                    setOpen(false)
-                                    setColour('#9381ff', '#ffeedd', '#b8b8ff')
-                                    navigate('/home')
-                                }}
-                            >
-                                <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: '#9381ff'}}>#9381ff</div>
-                                <div className="flex flex-col justify-center text-black rounded-md w-32 h-24" style={{backgroundColor: '#ffeedd'}}>#FFEEDD</div>
-                                <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: '#b8b8ff'}}>#B8B8FF</div>
-                            </button>
-                            <br />
-                            <button
-                                type="button"
-                                style={{gap: 10}}
-                                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-200 text-base font-medium text-black hover:bg-orange-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                onClick={() => {
-                                    setOpen(false)
-                                    setColour('#D4A373', '#faedcd', '#606c38')
-                                    navigate('/home')
-                                }}
-                            >
-                                <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: '#D4A373'}}>#D4A373</div>
-                                <div className="flex flex-col justify-center text-black rounded-md w-32 h-24" style={{backgroundColor: '#faedcd'}}>#FAEDCD</div>
-                                <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: '#606c38'}}>#606C38</div>
-                            </button>
+                            {colourData.map((item) => {
+                                return (
+                                    <>
+                                    <button
+                                        type="button"
+                                        style={{gap: 10}}
+                                        className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-orange-200 text-base font-medium text-black hover:bg-orange-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 sm:ml-3 sm:w-auto sm:text-sm"
+                                        onClick={() => {
+                                            setOpen(false)
+                                            setColour(item.c1, item.c2, item.c3)
+                                            navigate('/home')
+                                        }}
+                                    >
+                                        <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: item.c1}}>{item.c1}</div>
+                                        <div className="flex flex-col justify-center text-black rounded-md w-32 h-24" style={{backgroundColor: item.c2}}>{item.c2}</div>
+                                        <div className="flex flex-col justify-center text-white rounded-md w-32 h-24" style={{backgroundColor: item.c3}}>{item.c3}</div>
+                                    </button>
+                                    <br />
+                                    </>
+                                )
+                            })}
                         </div>
                         
                     </div>
